9e913e8318407ddf343156073db7572af775d2f7
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-10 17:15:02 -0500

    webpack-dev-server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return <h1>Hello World!!</h1>
   }
 }
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,8 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    port: 9000
+  }
 })

8066992f0beb40565ffd6a8acb2887c834329f43
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-10 15:08:06 -0500

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -18,5 +19,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
 }

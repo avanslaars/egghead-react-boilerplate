5e256bc276785982dd1c091ca0bc015f0a997b3a
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-10 15:59:45 -0500

    separate webpack configs

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),

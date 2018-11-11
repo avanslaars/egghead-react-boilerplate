9a99e905ef19e0dcee8e879ce68a20baf2599bef
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-11 09:17:56 -0500

    support css with css and style loaders

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -17,6 +17,11 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
           plugins: ['@babel/plugin-proposal-class-properties']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
       }
     ]
   },

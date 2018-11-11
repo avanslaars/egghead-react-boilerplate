39562e9134552c26960108f90d7288d565a4644c
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-11 13:20:21 -0500

    Deduped npm dev scripts

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     const {count} = this.state
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>Hello World.</h1>
         <h2 className={count > 10 ? 'warning' : null}>
           Count: {count}
         </h2>

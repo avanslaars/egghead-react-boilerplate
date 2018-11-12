582921f9cf99ea9f062d91e7510b576a257b1690
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-12 18:26:57 -0500

    configured preset-env and babel polyfill

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,7 +14,15 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [['@babel/preset-env', {
+            targets: [
+              'last 2 versions',
+              'not dead',
+              'not < 2%',
+              'not ie 11'
+            ],
+            useBuiltIns: 'entry'
+          }], '@babel/preset-react'],
           plugins: [
             'react-hot-loader/babel',
             '@babel/plugin-proposal-class-properties'

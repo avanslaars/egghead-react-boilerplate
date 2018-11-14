58fc8092d31e8bb0e68ffae9d4fb2d61b59d2c4e
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-13 19:21:21 -0500

    Async module loading added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 
+const Warning = React.lazy(() => import('./Warning'))
+
 class App extends React.Component {
   state = {
     count: 0
@@ -24,6 +26,11 @@ class App extends React.Component {
         </h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
+        {count > 10 ?
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+          : null}
       </div>
     )
   }
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -25,7 +25,8 @@ module.exports = {
           }], '@babel/preset-react'],
           plugins: [
             'react-hot-loader/babel',
-            '@babel/plugin-proposal-class-properties'
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-syntax-dynamic-import'
           ]
         }
       },

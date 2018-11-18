c4248aae33c858f07bdcab4fbe12c0c0c4c13d68
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-18 14:02:09 -0500

    Testing a React component

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 import App from './App'
 
 describe('App', () => {
   it('Renders without error', () => {
-    render(<App/>)
+    render(<App />)
   })
 })

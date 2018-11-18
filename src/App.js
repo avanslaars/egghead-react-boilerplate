7dbf777c570827526a48fde8edd3462e3800af4c
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-18 11:38:55 -0500

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
-
   render() {
-    const { count } = this.state
     return (
       <div>
         <h1>Hello World.</h1>
-        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     )
   }
--- src/Warning.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Warning = () => <span className={'warning'}>Take it easy!</span>
-
-Warning.displayName = 'Warning'
-
-export default Warning

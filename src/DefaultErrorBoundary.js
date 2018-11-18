44f017312c91a78165a02b15f252563cf27aa626
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-18 10:07:33 -0500

    Added DefaultErrorBoundary

---
+++ src/DefaultErrorBoundary.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+export default class DefaultErrorBoundary extends React.Component {
+  state = {
+    isError: false
+  }
+
+  static getDerivedStateFromError() {
+    return { isError: true }
+  }
+
+  static propTypes = {
+    children: propTypes.node.isRequired
+  }
+
+  render() {
+    const { isError } = this.state
+    const { children } = this.props
+    return isError ? <div>Something went wrong!</div> : children
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import App from './App'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 import './styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 )

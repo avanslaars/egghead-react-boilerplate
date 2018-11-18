1fbaa01c9c70375822b73a82cf55166677aa978b
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-18 13:56:12 -0500

    Simple test

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { render } from 'react-testing-library'
 import App from './App'
 
 describe('App', () => {
-  it('Renders without error', () => {
-    render(<App/>)
+  it('Runs and passes', () => {
+    expect(true).toBe(true)
   })
 })
